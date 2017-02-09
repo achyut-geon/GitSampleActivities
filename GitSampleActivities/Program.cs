634d52a87931820316a41d31738b68ae91dd6e0d
Achyut Menon <achyut.menon@gadgeon.com>
2017-02-09 19:41:01 +0530

    Initial commit

--- GitSampleActivities/Program.cs
+++ GitSampleActivities/Program.cs
@@ -10,6 +10,8 @@ namespace GitSampleActivities
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
         }
     }
 }
